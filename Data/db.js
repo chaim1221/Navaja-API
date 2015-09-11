3b3996b58e43c2527f2e4fe6813f65ef46331ca4
Chaim Eliyah <chaimeliyah@gmail.com>
2015-09-11 15:04:36 -0700

    Wrote an empty mocha test, yayyy...

--- Data/db.js
+++ Data/db.js
@@ -1,5 +1,14 @@
-var pg = require('pg').native;
+var pg = require('pg').native; // native pool, not a per-instance request
 var connectionString = "postgresql://machetedb_app_user:replace_me@localhost:5432/machetedb";
 
+// data should include userId
+function create(table, data) {
+}
 
+function get(table, id) {
+}
 
+function update(table, data) {
+}
+
+// deletion not supported via API (we keep everything)
---
+++ Test/Integration/Data/db.spec.js
@@ -0,0 +1,16 @@
+var assert = require("assert")
+  , http = require("http")
+  , should = require("should")
+  , request = require("request")
+  , exec = require("child_process").exec;
+
+describe("db", function() {
+    
+    describe("method: create", function () {
+        //before(function(done) { });
+        //after(function(done) { });
+        it("should create a record for a given existing table", function() {
+            //done();
+        });
+    });
+});
