e6aa217b2f1835491891ef41861dcf7586c20fb4
Chaim Eliyah <chaimeliyah@gmail.com>
2015-09-16 16:05:23 -0700

    First test PASSING! Thanks @Yuri Zarubin

--- Data/db.js
+++ Data/db.js
@@ -3,9 +3,12 @@ config = require("./../config.js");
 
 module.exports = { 
     query: function(text, values, cb) {
+        console.log("I get to this in Mocha");
         pg.connect(config.connectionString, function(err, client, done) {
+            console.log("I never get here");
             if (err) return console.error("error connecting to postgres: ", err);
             client.query(text, values, function(err, result) {
+                console.log("I most certainly never get here");
                 done();
                 cb(err, result);
             })
@@ -18,7 +21,7 @@ leave this for now, since there aren't any tests....
 
 node
 var db = require ("./db.js");
-db.query("", {}, function (err, result) {
+db.query("insert into sometable(id, value) values(1, \"blah\")", {}, function (err, result) {
                 if (err) { console.error ("db errored out man"); }
                 console.log("no error...");
                 console.log(result);
--- test/Integration/Data/db.spec.js
+++ test/Integration/Data/db.spec.js
@@ -1,18 +1,18 @@
-var assert = require("assert")
-  , should = require("should")
-  , expect = require("expect")
-  , db = require("./../../../Data/db.js");
+var db = require("./../../../Data/db.js");
 
 // These tests assume you have run the scripts in the -SQL repo
 describe("module: db", function() {
-    describe("method: read", function() {
-        it("returns a record when read from the database", function () {
-            db.query("select 1", {}, function (err, result) {
-                if (err) { console.error ("db errored out man"); }
-                console.log("no error...");
-                console.log(result);
-            });
-        });
+    it("provides a wrapper for the execution of queries", function (done) {
+        db.query("insert into employer.profile \
+            (id, returncustomer, receiveupdates, name, email, password, active) \
+            values (4, true, true, 'Chaim Eliyah', 'ce@spam.org', 'change_me', true)", {}, 
+            function (err, stdout, stderr) { 
+                console.log(err || ""); 
+                console.log(stdout || ""); 
+                console.log(stderr || "");
+                done();
+            }
+        );
     });
 }); 
 
