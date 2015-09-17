86de5bfa0744f9a219bea7f3e3a27026f6c47531
Chaim Eliyah <chaimeliyah@gmail.com>
2015-09-16 20:18:38 -0700

    Adding update test.

--- test/Integration/Data/db.spec.js
+++ test/Integration/Data/db.spec.js
@@ -1,18 +1,29 @@
 var db = require("./../../../Data/db.js");
+var attempt = "31";
 
-// These tests assume you have run the scripts in the -SQL repo
 describe("module: db", function() {
     describe("provides a wrapper for the execution of queries", function () {
     
         it("can insert rows", function(done) {
             db.query("insert into employer.profile \
                 (id, returncustomer, receiveupdates, name, email, password, active) \
-                values (30, true, true, 'Chaim Eliyah', 'ce@spam.org', 'change_me', true)", 
+                values (" + attempt + ", true, true, 'Chaim Eliyah', 'ce@spam.org', 'change_me', true)", 
                 function (err, result) { 
                     done();
                     if (err) {  
                         throw new Error(err);
                     }
+                    console.log(result);
+                }
+            );
+        });
+        
+        it("can update rows", function(done) {
+            db.query("update employer.profile\
+                set active = false where id = " + attempt,
+                function (err, result) {
+                    done();
+                    if (err) throw new Error(err);
                     console.log(result);
                 }
             );
