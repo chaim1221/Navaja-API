7efae2176f873c8e8b987589e09db61129d64d7b
Chaim Eliyah <chaimeliyah@gmail.com>
2015-09-16 19:43:48 -0700

    Simplest form I've been able to achieve...

--- Data/db.js
+++ Data/db.js
@@ -2,29 +2,15 @@ pg = require("pg");
 config = require("./../config.js");
 
 module.exports = { 
-    query: function(text, values, cb) {
-        console.log("I get to this in Mocha");
+    query: function(text, callback) {
         pg.connect(config.connectionString, function(err, client, done) {
-            console.log("I never get here");
             if (err) return console.error("error connecting to postgres: ", err);
-            client.query(text, values, function(err, result) {
-                console.log("I most certainly never get here");
+
+            client.query(text, function(error, result) {
                 done();
-                cb(err, result);
-            })
+                callback(error, result);
+            });
         });
     }
 }
-
-/*
-leave this for now, since there aren't any tests....
-
-node
-var db = require ("./db.js");
-db.query("insert into sometable(id, value) values(1, \"blah\")", {}, function (err, result) {
-                if (err) { console.error ("db errored out man"); }
-                console.log("no error...");
-                console.log(result);
-            });
-*/
 
--- test/Integration/Data/db.spec.js
+++ test/Integration/Data/db.spec.js
@@ -2,17 +2,21 @@ var db = require("./../../../Data/db.js");
 
 // These tests assume you have run the scripts in the -SQL repo
 describe("module: db", function() {
-    it("provides a wrapper for the execution of queries", function (done) {
-        db.query("insert into employer.profile \
-            (id, returncustomer, receiveupdates, name, email, password, active) \
-            values (4, true, true, 'Chaim Eliyah', 'ce@spam.org', 'change_me', true)", {}, 
-            function (err, stdout, stderr) { 
-                console.log(err || ""); 
-                console.log(stdout || ""); 
-                console.log(stderr || "");
-                done();
-            }
-        );
+    describe("provides a wrapper for the execution of queries", function () {
+    
+        it("can insert rows", function(done) {
+            db.query("insert into employer.profile \
+                (id, returncustomer, receiveupdates, name, email, password, active) \
+                values (29, true, true, 'Chaim Eliyah', 'ce@spam.org', 'change_me', true)", 
+                function (err, result) { 
+                    done();
+                    if (err) {  
+                        throw new Error(err);
+                    }
+                    console.log(result);
+                }
+            );
+        });
     });
 }); 
 
