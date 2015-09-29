77d811f28e38d8ea71d0bbf711b9ab0222910bdf
Chaim Eliyah <chaimeliyah@gmail.com>
2015-09-29 15:27:23 -0700

    Adding employerBusinessRepo

--- test/integration/data/employer/business.spec.js
+++ test/integration/data/employer/business.spec.js
@@ -1,1 +1,73 @@
-/**/
+var assert = require('chai').assert;
+var EmployerProfileRepository = require(__dirname + '/../../../../data/employer/profile.js').Repository;
+var EmployerBusinessRepository = require(__dirname + '/../../../../data/employer/business.js').Repository;
+
+describe('When we want to keep track of an employer business', function () {
+    var employerBusinessRepository = new EmployerBusinessRepository();
+    
+    var business = {
+        profileId: null,
+        name: "Barry's Mules",
+        federalTaxId: "01-234-5678",
+        active: true
+    }
+    
+    before(function (done) {    
+        var employerProfileRepository = new EmployerProfileRepository();
+
+        var profile = {
+            returnCustomer: true,
+            receiveUpdates: true, 
+            name: 'Barack Obama', 
+            email: 'obama@spam.org', 
+            password: 'change_me', 
+            active: true
+        };
+
+        employerProfileRepository.add(profile).then(function (returnedValue) {
+            business.profileId = parseInt(returnedValue);
+            done();
+        });
+    });
+    
+    describe("Repository: EmployerBusinessRepository", function () {
+        it('Can add employer businesses', function (done) {
+            employerBusinessRepository.add(business).then(function (returnedValue) {
+                assert.ok(returnedValue);
+                business.id = parseInt(returnedValue);
+                done();
+            });
+        });
+        
+        // TODO: Test multiple businesses for a single employer
+        // TODO: Add functionality to return all active businesses for a single employer
+    
+        it('Can get employer businesses by id', function (done) {
+            employerBusinessRepository.getById(business.id).then(function (result) {
+                assert.equal(result.id, business.id);
+                assert.equal(result.profileId, business.profileId);
+                assert.equal(result.name, business.name);
+                assert.equal(result.federalTaxId, business.federalTaxId);
+                assert.equal(result.active, business.active);
+                done();
+            });
+        });
+        
+        it('Can update employer businesses', function (done) {
+            business.active = false;
+            employerBusinessRepository.update(business).then(function (returnedValue) {
+                assert.ok(returnedValue);
+                done();
+            });
+        });
+        
+        it("can delete rows", function(done) {
+            employerBusinessRepository.remove(business.id)
+                .then(function(rowsAffected) {
+                    assert.isAbove(rowsAffected, 0);
+                    done();
+                });
+        });
+    });
+});
+
